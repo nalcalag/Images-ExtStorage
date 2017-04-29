f56041f59429458747dda5ad88f07ce747b1c01c
Noelia <nalcalag@uoc.edu>
2017-04-29 20:11:23 +0100

    Save and Delete methods done

--- app/src/main/java/edu/uoc/android/imageapp/ImageActivity.java
+++ app/src/main/java/edu/uoc/android/imageapp/ImageActivity.java
@@ -1,8 +1,82 @@
 package edu.uoc.android.imageapp;
 
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Created by nalcalag on 29/04/2017.
  */
 
 public class ImageActivity {
+
+    private View view;
+    private static String directory = Environment.getExternalStorageDirectory()+"/UOCImageApp";
+    private static String imgName = "imageApp.jpg";
+
+
+    public ImageActivity(View view) {
+        this.view = view;
+    }
+
+    public void saveImage(Bitmap bitmap) {
+
+        File uocFile = new File(directory);
+        if (!uocFile.exists()) {
+            uocFile.mkdir();
+        }
+
+        // Comprueba si el fichero de destino ya existe, en este caso lo borra.
+        File file = new File(directory, imgName);
+        if (file.exists()) {
+            file.delete();
+        }
+
+        FileOutputStream fos;
+
+        try {
+            fos = new FileOutputStream(file);
+
+            // Save Bitmap in /UOCImageApp Directory
+            if (fos!= null) {
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                fos.close();
+                Snackbar mySnackbar = Snackbar.make(view.findViewById(R.id.root),
+                        R.string.save, Snackbar.LENGTH_SHORT);
+                mySnackbar.show();
+            }
+        }
+        catch (IOException ex) {
+            Log.e("IOException", ex.toString());
+            Snackbar mySnackbar = Snackbar.make(view.findViewById(R.id.root),
+                    R.string.error_save, Snackbar.LENGTH_SHORT);
+            mySnackbar.show();
+        }
+    }
+
+    public void deleteImage() {
+
+        File file = new File(directory + imgName);
+
+        if (file.exists()) {
+            file.delete();
+        }
+        Snackbar mySnackbar = Snackbar.make(view.findViewById(R.id.root),
+                R.string.delete, Snackbar.LENGTH_SHORT);
+        mySnackbar.show();
+    }
+
+    public static String getDirectory() {
+        return directory;
+    }
+
+    public static String getImgName() {
+        return imgName;
+    }
 }
--- app/src/main/java/edu/uoc/android/imageapp/MainActivity.java
+++ app/src/main/java/edu/uoc/android/imageapp/MainActivity.java
@@ -1,10 +1,12 @@
 package edu.uoc.android.imageapp;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -12,6 +14,7 @@ import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,11 +22,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -38,6 +40,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView tvMessage;
     //
     String mCurrentPhotoPath;
+    String uocPhotoPath;
+    ImageActivity imageActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +56,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Set listeners
         buttonOpenImage.setOnClickListener(this);
+
+        // Check if there is an image saved in the phone storage and print it
+        uocPhotoPath = imageActivity.getDirectory() + "/" + imageActivity.getImgName();
+        File file = new File(imageActivity.getDirectory() + "/" + imageActivity.getImgName());
+
+        if (file.exists()) {
+
+            mImageView.setImageBitmap(BitmapFactory.decodeFile(uocPhotoPath));
+            tvMessage.setVisibility(View.INVISIBLE);
+        }
     }
 
     @Override
@@ -81,6 +95,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                               REQUEST_PERMISSION_STORAGE_DELETE);
         }
         // TODO: show dialog if image file exists
+
+        if (mImageView != null) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Delete Image")
+                    .setMessage("Do you want to delete this image?")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // continue with delete
+                            imageActivity = new ImageActivity(rootView);
+                            imageActivity.deleteImage();
+                            mImageView.setImageDrawable(null);
+                            tvMessage.setVisibility(View.VISIBLE);
+                        }
+                    })
+                    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // do nothing
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
     }
 
     private void onSaveMenuTap() {
@@ -91,6 +127,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                               REQUEST_PERMISSION_STORAGE_SAVE);
         }
         // TODO save the image
+        imageActivity = new ImageActivity(rootView);
+        //Check if there is an image
+        if (mImageView.getDrawable() != null) {
+            imageActivity.saveImage(((BitmapDrawable)mImageView.getDrawable()).getBitmap());
+        } else {
+            Toast.makeText(this, "No image to save", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private boolean hasPermissionsToWrite() {
@@ -101,28 +144,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v == buttonOpenImage) {
-            // TODO: launching an intent to get an image from camera app
 
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            takePicture();
+        }
+    }
 
-            // Ensure that there's a camera activity to handle the intent
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                // Create the File where the photo should go
-                File photoFile = null;
-                try {
-                    photoFile = createImageFile("temp");
-                } catch (IOException ex) {
-                    // Error occurred while creating the File
+    //Method to take a picture from Camera App and save in a temp File
+    private void takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-                }
-                // Continue only if the File was successfully created
-                if (photoFile != null) {
-                    Uri photoURI = FileProvider.getUriForFile(this,
-                            "com.example.android.fileprovider",
-                            photoFile);
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-                }
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile("temp");
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
     }
@@ -146,17 +193,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
 
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-            Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, options);
-            mImageView.setImageBitmap(bitmap);
+            setPic();
 
+            //Set TextView when there is an image
             if (mImageView !=null) {
                 tvMessage.setVisibility(View.INVISIBLE);
             } else {
                 tvMessage.setVisibility(View.VISIBLE);
             }
         }
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        mImageView.setImageBitmap(bitmap);
     }
 
     @Override
